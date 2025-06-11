fffe1a69910139762df20d0eaa93457c1b0f1d45
mcbarinov <mcbarinov@proton.me>
2025-06-11 10:34:00 +0300

    add __all__ for re-export

--- src/mm_print/__init__.py
+++ src/mm_print/__init__.py
@@ -1,5 +1,7 @@
 from .output import fatal as fatal
-from .output import print_json as json  # noqa: F401
-from .output import print_plain as plain  # noqa: F401
-from .output import print_table as table  # noqa: F401
-from .output import print_toml as toml  # noqa: F401
+from .output import print_json as json
+from .output import print_plain as plain
+from .output import print_table as table
+from .output import print_toml as toml
+
+__all__ = ["fatal", "json", "plain", "table", "toml"]
